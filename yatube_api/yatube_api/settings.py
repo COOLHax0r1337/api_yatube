4bf4018f22d7fb91670e6021479d2421f23e830d
Philip Kurtakov <zj6lyp03r9oc@mail.ru>
2023-02-07 21:51:53 +0300

    getenv fix

--- yatube_api/yatube_api/settings.py
+++ yatube_api/yatube_api/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'm%(5u7nv9j2%@3xb%#c3p-$9&0$kq$j6l@9+@ogairu48a+dy+'
+SECRET_KEY = os.getenv('SECRET_KEY', '123333')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
