fbe918b7dc32d3037dcf836e36e2bf1956969e72
Philip Kurtakov <zj6lyp03r9oc@mail.ru>
2023-02-08 16:49:58 +0300

    models fix

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+POST_LENGTH = 15
+
 User = get_user_model()
 
 
@@ -18,7 +20,7 @@ class Post(models.Model):
     pub_date = models.DateTimeField(
         'Дата публикации', auto_now_add=True,
     )
-    author = models.ForeignKey(User, on_delete=models.CASCADE,)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(
         upload_to='posts/', null=True, blank=True
     )  # поле для картинки
@@ -31,12 +33,12 @@ class Post(models.Model):
         default_related_name = 'posts'
 
     def __str__(self):
-        return self.text[:15]
+        return self.text[:POST_LENGTH]
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE,)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE,)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
     text = models.TextField()
     created = models.DateTimeField(
         'Дата добавления', auto_now_add=True, db_index=True,
